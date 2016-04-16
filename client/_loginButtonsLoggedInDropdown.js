facb4bb6363a7ae05a23cf9b9ab59027f4e447cd
Matthew T. Stewart <matthew.stewart.mi@gmail.com>
2016-04-15 21:40:23 -0400

    fix profile button

--- client/_loginButtonsLoggedInDropdown.js
+++ client/_loginButtonsLoggedInDropdown.js
@@ -1,5 +1,7 @@
 Template._loginButtonsLoggedInDropdown.events({
     'click #login-buttons-view-profile': function(event) {
         //Router.go('userPage', {_username: Meteor.user().username});
+		var profilePath = '/users/' + Meteor.user().username;
+		FlowRouter.go(profilePath);
     }
 });
